990bd141b6b1e67c51949619de39157ca7ee9560
Viktor Grandgeorg <info@intelligibel.de>
2014-03-20 11:01:04 +0100

    - blockquote für tinyMCE hinzugefügt - transition in css page nav auskommentiert

--- admin/themes/default/javascripts/globals.js
+++ admin/themes/default/javascripts/globals.js
@@ -19,7 +19,7 @@ if (!Omeka) {
             theme_advanced_toolbar_location: "top",
             theme_advanced_statusbar_location: "none",
             theme_advanced_toolbar_align: "left",
-            theme_advanced_buttons1: "bold,italic,underline,|,justifyleft,justifycenter,justifyright,|,bullist,numlist,|,link,formatselect,code",
+            theme_advanced_buttons1: "bold,italic,underline,|,justifyleft,justifycenter,justifyright,|,bullist,numlist,blockquote,|,link,formatselect,code",
             theme_advanced_buttons2: "",
             theme_advanced_buttons3: "",
             plugins: "paste,inlinepopups,media",
